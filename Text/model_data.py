519e71ee408479a41111fe2fd053d8510986a0ae
Chunyuan-Jin <cjin46@wisc.edu>
2019-11-14 23:16:44 -0600

    change

--- Text/model_data.py
+++ Text/model_data.py
@@ -3,21 +3,34 @@
 
 import json
 
-f = open('raw_text.json')
-f1 = open('process_nontext.json')
-f2 = open('model_data.json', 'w')
+
+f = open('process_nontext.json')
+f2 = open('non.json', 'w')
 while True:
     line = f.readline()
-    line1 = f1.readline()
     if not line:
         break
     new_dic = dict(json.loads(line))
-    new_dic1 = dict(json.loads(line1))
-    pd = new_dic['text']
-    new_dic1['text'] = pd
-    json.dump(new_dic1, f2)
+    new_dic.pop("user_id")
+    new_dic.pop("business_id")
+    new_dic.pop("stars")
+    new_dic.pop("useful")
+    new_dic.pop("date")
+    new_dic.pop("name")
+    new_dic.pop("city")
+    new_dic.pop("state")
+    new_dic.pop("bus_stars")
+    new_dic.pop("bus_review_count")
+    new_dic.pop("user_name")
+    new_dic.pop("user_review_count")
+    new_dic.pop("yelping_since")
+    new_dic.pop("user_useful")
+    new_dic.pop("friends")
+    new_dic.pop("fans")
+    new_dic.pop("average_stars")
+    new_dic.pop("compliment")
+    json.dump(new_dic, f2)
     f2.write("\n")
 f.close()
-f1.close()
 f2.close()
 
