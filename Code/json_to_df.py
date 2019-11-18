f4a0e9f2b2eba1f6a9cb96904868248609e5304d
hao pan <hpan56@wisc.edu>
2019-11-18 10:15:01 +0800

    Multiple changes

---
+++ Code/json_to_df.py
@@ -0,0 +1,47 @@
+import json
+import pandas as pd
+
+
+def json_to_df_lines(path, lines=100, skip=0):
+    with open(path, encoding="utf-8") as f:
+        dic = {}
+        for i in range(1,lines+skip+1):
+            line = f.readline()
+            if i <= skip:
+                continue
+            dic_new = dict(json.loads(line))
+            if i == skip+1:
+                dic = dic_new
+                for key in dic:
+                    dic[key] = [dic[key]]
+            else:
+                for key in dic:
+                    dic[key].append(dic_new[key])
+        f.close()
+        dic = pd.DataFrame(dic)
+        return dic
+
+    
+def json_to_df_exhaust(path,skip=0):
+    with open(path, encoding="utf-8") as f:
+        dic = {}
+        lines = f.readlines()
+        f.close()
+        i = 0
+        for line in lines:
+            i += 1
+            if i <= skip:
+                continue
+            dic_new = dict(json.loads(line))
+            if i == 1+skip:
+                dic = dic_new
+                for key in dic:
+                    dic[key] = [dic[key]]
+            else:
+                for key in dic:
+                    try:
+                        dic[key].append(dic_new[key])
+                    except:
+                        dic[key].append(None)
+        dic = pd.DataFrame(dic)
+        return dic
